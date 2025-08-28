47f2268172efbbb68b821070bea0b0996e76095c
Nour Moftah <noursaad2213@gmail.com>
2025-08-28 22:58:10 +0300

    Profile Form With Formik and Yup

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import { ColorModeContext, useMode } from "./theme";
-import { CssBaseline, ThemeProvider } from "@mui/material"; 
-
-import Topbar from "./scenes/global/Topbar"; 
-
-import Topbar from "./scenes/global/Topbar"; //change t to cap
-
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import Topbar from "./scenes/global/Topbar";
 import Sidebar from "./scenes/global/Sidebar";
 import Dashboard from "./scenes/dashboard";
 import Team from "./scenes/team";
-
 import Contacts from "./scenes/contacts";
 import Invoices from "./scenes/invoices";
+import Form from "./scenes/form";
 // import Bar from "./scenes/bar";
-// import Form from "./scenes/form";
 // import Line from "./scenes/line";
 // import Pie from "./scenes/pie";
 // import FAQ from "./scenes/faq";
@@ -29,16 +24,16 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
-          <Sidebar/>
+          <Sidebar />
           <main className="content">
             <Topbar />
             <Routes>
               <Route path="/" element={<Dashboard />} />
-               <Route path="/team" element={<Team />} />
+              <Route path="/team" element={<Team />} />
               <Route path="/contacts" element={<Contacts />} />
-             <Route path="/invoices" element={<Invoices />} />
-            {/*   <Route path="/bar" element={<Bar />} />
+              <Route path="/invoices" element={<Invoices />} />
               <Route path="/form" element={<Form />} />
+              {/*   <Route path="/bar" element={<Bar />} />
               <Route path="/line" element={<Line />} />
               <Route path="/pie" element={<Pie />} />
               <Route path="/faq" element={<FAQ />} />
