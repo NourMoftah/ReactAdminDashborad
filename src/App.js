c23c2b5bab96460c94e29cfab46a9bd84d870ba3
Nour Moftah <noursaad2213@gmail.com>
2025-09-04 10:50:22 +0300

    adding BarChart Section

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Invoices from "./scenes/invoices";
 import Form from "./scenes/form";
 import Calendar from "./scenes/calendar";
 import FAQ from "./scenes/faq";
-// import Bar from "./scenes/bar";
+import Bar from "./scenes/bar";
 // import Line from "./scenes/line";
 // import Pie from "./scenes/pie";
 // import Geography from "./scenes/geography";
@@ -27,21 +27,20 @@ function App() {
           <Sidebar />
           <main className="content">
             <Topbar />
-           <Routes>
-  <Route path="/" element={<Dashboard />} />
-  <Route path="/team" element={<Team />} />
-  <Route path="/contacts" element={<Contacts />} />
-  <Route path="/invoices" element={<Invoices />} />
-  <Route path="/form" element={<Form />} />
-  <Route path="/calendar" element={<Calendar />} />
-  <Route path="/faq" element={<FAQ />} />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/team" element={<Team />} />
+              <Route path="/contacts" element={<Contacts />} />
+              <Route path="/invoices" element={<Invoices />} />
+              <Route path="/form" element={<Form />} />
+              <Route path="/calendar" element={<Calendar />} />
+              <Route path="/faq" element={<FAQ />} />
 
-  {/* <Route path="/bar" element={<Bar />} /> */}
-  {/* <Route path="/geography" element={<Geography />} /> */}
-  {/* <Route path="/line" element={<Line />} /> */}
-  {/* <Route path="/pie" element={<Pie />} /> */}
-</Routes>
-
+              <Route path="/bar" element={<Bar />} />
+              {/* <Route path="/geography" element={<Geography />} /> */}
+              {/* <Route path="/line" element={<Line />} /> */} 
+              {/* <Route path="/pie" element={<Pie />} /> */}
+            </Routes>
           </main>
         </div>
       </ThemeProvider>
